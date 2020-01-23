6dc6b86714476c22df9615d636532994a5144b12
MinexAutomation <minex.automation@gmail.com>
2020-01-23 11:47:53 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Stockholm.Tiros.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Stockholm.Tiros.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTextStreamSerializer<T>(this IServiceCollection services)
+        {
+            services.AddSingleton<IStreamSerializer<T>, TextStreamSerializer<T>>();
+
+            return services;
+        }
+    }
+}
