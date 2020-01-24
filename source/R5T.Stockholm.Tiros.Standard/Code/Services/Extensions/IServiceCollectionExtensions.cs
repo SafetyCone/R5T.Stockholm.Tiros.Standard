5ad43776cfea0bdcd42c038dad752d09b51b9fcc
MinexAutomation <minex.automation@gmail.com>
2020-01-24 15:51:01 -0800

    Added IStreamSerializerOptions<T> service.

--- source/R5T.Stockholm.Tiros.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Stockholm.Tiros.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Stockholm.Default;
+
 
 namespace R5T.Stockholm.Tiros.Standard
 {
@@ -9,7 +11,10 @@ namespace R5T.Stockholm.Tiros.Standard
     {
         public static IServiceCollection AddTextStreamSerializer<T>(this IServiceCollection services)
         {
-            services.AddSingleton<IStreamSerializer<T>, TextStreamSerializer<T>>();
+            services
+                .AddSingleton<IStreamSerializer<T>, TextStreamSerializer<T>>()
+                .AddSingleton<IStreamSerializerOptions<T>, DefaultStreamOperatorOptions<T>>()
+                ;
 
             return services;
         }
